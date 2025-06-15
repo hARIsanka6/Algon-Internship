3a4d47b689657a59ac7e2f5a1aef65158d5a37b0
Harisankar <harisankarlflower@gmail.com>
2025-06-16 04:49:23 +0530

    Added Day2 works

--- Day2-Assignment/Pattern_Printing/pattern5.py
+++ Day2-Assignment/Pattern_Printing/pattern5.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 num = int(input("Enter the number of rows:"))
 for i in range(num,0,-1):
@@ -16,23 +15,4 @@ for i in range(num,0,-1):
 
 
 
-=======
-
-num = int(input("Enter the number of rows:"))
-for i in range(num,0,-1):
-    for j in range(0,i):
-        print("*",end="");
-    print();
-
-
-'''
-*****
-****
-***
-**
-*
-
-
-
->>>>>>> 4d32e8f (Added Day2 works)
 '''
